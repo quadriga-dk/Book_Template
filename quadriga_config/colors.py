1d29877fd09589007439ba38d82f752b5ab4ab7d
Hannes Schnaitter <git@schnaitter.info>
2024-07-17 15:51:04 +0200

    Add: Konfigurationsmodul für koordinierte Einstellungen

---
+++ quadriga_config/colors.py
@@ -0,0 +1,7 @@
+"""This submodule contains all QUADRIGA color presets for the various libraries used."""
+
+jupyterquiz = {
+    "--jq-multiple-choice-bg": "#04345a",
+    "--jq-many-choice-bg": "#04345a",
+    "--jq-numeric-bg": "#04345a",
+}
