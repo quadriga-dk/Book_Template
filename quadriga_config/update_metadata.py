357daf70e4b4b03205cd4c98232de3c46fdd6401
Hannes Schnaitter <git@schnaitter.info>
2025-03-10 15:43:29 +0100

    wip: first update_metadata.py that changes something in metadata.yml

--- quadriga_config/update_metadata.py
+++ quadriga_config/update_metadata.py
@@ -8,7 +8,38 @@ def update_metadata():
     # Load CITATION.cff
     with open('CITATION.cff', 'r') as f:
         citation_data = yaml.safe_load(f)
-    print(citation_data["preferred-citation"])
+    
+    # Load _toc.yml
+    with open('_toc.yml', 'r') as f:
+        toc_data = yaml.safe_load(f)
+    
+    # Load existing metadata.yml
+    with open('metadata.yml', 'r') as f:
+        metadata = yaml.safe_load(f)
+    
+    # Update metadata fields based on citation and toc
+    if 'title' in citation_data:
+        metadata['title'] = citation_data['title']
+    
+    if 'authors' in citation_data:
+        # Convert citation authors format to metadata authors format
+        metadata_authors = []
+        for author in citation_data['authors']:
+            author_entry = {}
+            if 'given-names' in author:
+                author_entry['given-name'] = author['given-names']
+            if 'family-names' in author:
+                author_entry['familyname'] = author['family-names']
+            if 'orcid' in author:
+                author_entry['orcid'] = author['orcid']
+            if 'affiliation' in author:
+                author_entry['affiliations'] = [author['affiliation']]
+            metadata_authors.append(author_entry)
+        metadata['authors'] = metadata_authors
 
+    # Save updated metadata.yml
+    with open('metadata.yml', 'w') as f:
+        f.write("# yaml-language-server: $schema=quadriga-schema.json")
+        yaml.dump(metadata, f, sort_keys=False, default_flow_style=False)
 if __name__ == "__main__":
     update_metadata()
