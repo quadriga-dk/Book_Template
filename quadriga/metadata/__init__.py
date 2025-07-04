d1b09505b8378f3c0abaaf7c1eec79170702ebff
Hannes Schnaitter <git@schnaitter.info>
2025-07-04 10:46:21 +0200

    chore: reformat python code in quadriga using black

--- quadriga/metadata/__init__.py
+++ quadriga/metadata/__init__.py
@@ -5,12 +5,7 @@ This subpackage contains scripts for managing metadata across different formats
 including CITATION.cff, CITATION.bib, and extracting metadata from _config.yml.
 """
 
-__all__ = [
-    "create_bibtex",
-    "update_citation_cff",
-    "extract_from_book_config",
-    "utils"
-]
+__all__ = ["create_bibtex", "update_citation_cff", "extract_from_book_config", "utils"]
 
 # Import the modules to make their functions available
 from . import create_bibtex
