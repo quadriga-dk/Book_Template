03d4cf6e459b6afae6a6b70bd34e48c4ba292e5d
Hannes Schnaitter <git@schnaitter.info>
2025-07-07 14:55:50 +0200

    fix: remove nvim script

--- setup_nvim.sh
+++
@@ -1,5 +0,0 @@
-# these commands should be used from inside the virtual environment of the project
-
-pip install -r nvim-requirments.txt
-
-python -m ipykernel install --user --name=Book_Template_venv --display-name="Book_Template .venv"
